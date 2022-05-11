bf6f6bd903d7530a3e8190ab32e791d41483752c
snitolya <92542355+snitolya@users.noreply.github.com>
2022-05-11 12:25:14 +0300

    Update Comments.js

--- BLOG/Comments.js
+++ BLOG/Comments.js
@@ -2,20 +2,22 @@ import { Fragment, useEffect , useState } from 'react';
 import Spinner from "./Spinner";
 
 
-//  export default function Comments(){
-//     const [comments, setComments] = useState([]);
-//     useEffect ( () => {
-//         async function fetchData(){
-//             const response = await fetch ('https://jsonplaceholder.typicode.com/comments')
-//             const data = await response.json();
-//             setComments(data)
-//         }
-//         fetchData();
-//     },[])
+export default function Comments(props){
+const [comments, setComments] = useState([]);
+const [isLoading, setLoading] = useState(true);
+    useEffect ( () => {
+        async function fetchData(){
+            const response = await fetch ('https://jsonplaceholder.typicode.com/comments')
+            const data = await response.json();
+            setComments(data)
+            setLoading (false);
+        })()
+        fetchData();
+    },[props.postId])
 
  
-//     return (
-//         <Fragment>
+    return (
+        <Fragment>
         
-//         </Fragment> );
-//  }
+        </Fragment> );
+ }
