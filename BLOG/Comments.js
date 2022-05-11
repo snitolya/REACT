ab011e021aa8cb108d465b7b4bfdf9b799cbd4db
snitolya <92542355+snitolya@users.noreply.github.com>
2022-05-11 22:16:56 +0300

    Update Comments.js

--- BLOG/Comments.js
+++ BLOG/Comments.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect , useState } from 'react';
+mport { Fragment, useEffect , useState } from 'react';
 import Spinner from './Spinner';
 
 
@@ -7,13 +7,12 @@ export default function Comments(props){
   const [isLoading, setLoading] = useState(true);
     useEffect ( () => {
         (async function fetchData(){
-            const response = await fetch (`https://jsonplaceholder.typicode.com/posts/${props.comment}`)
+            const response = await fetch (`https://jsonplaceholder.typicode.com/posts/1/comments`)
             const data = await response.json();
             setComments(data)
             setLoading(false);
         })();
     },[props.postId])
-    console.log(comments);
 
  
     return ( 
@@ -30,52 +29,3 @@ export default function Comments(props){
         </Spinner>
          </Fragment>)
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Fragment, useEffect , useState } from 'react';
-// import Spinner from './Spinner';
-
-
-// export default function Author(props){
-//   const [author, setAuthor] = useState({})
-//   const [isLoading, setLoading] = useState(true);
-//     useEffect ( () => {
-//         (async function fetchData(){
-//             const response = await fetch (`https://jsonplaceholder.typicode.com/users/${props.userId}`)
-//             const data = await response.json();
-//             setAuthor(data)
-//             setLoading(false);
-//         })();
-//     },[props.userId])
-
- 
-//     return ( 
-//   <Fragment>
-//         <Spinner show={isLoading}>
-//             <dl>
-//                 <dt>name</dt>
-//                 <dd>{author.name}</dd>
-//                 <dt>email</dt>
-//                 <dd>{author.email}</dd>
-//                 <dt>phone</dt>
-//                 <dd>{author.phone}</dd>
-//             </dl>
-//         </Spinner>
-//          </Fragment>)
-//  }
