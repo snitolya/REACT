294b0d86e74a7fe9d3fe8bb2b9f1b738d0cfea3d
snitolya <92542355+snitolya@users.noreply.github.com>
2022-05-16 20:40:50 +0300

    Update ShoppingCart.js

--- ShoppingCart/ShoppingCart.js
+++ ShoppingCart/ShoppingCart.js
@@ -9,15 +9,15 @@ import CartItem from './CartItem';
     
     useEffect ( () => {
         async function fetchData(){
-            const response = await fetch ('http://192.168.0.200:3000/products')
+            const resp = await fetch ('http://192.168.0.200:3000/products')
             const data = await response.json();
-            setProducts(data)
+            setProducts(data);
         }
         fetchData();
     },[])
 
-    const ChangeItemCount = (id,count)=>{
-        setProducts((products)=>{
+    const changeItemCount = (id,count)=>{
+        setProducts((products)=>
           products.map(product=>{
               if(product.id===id){
                   return{...product,count};
@@ -26,10 +26,24 @@ import CartItem from './CartItem';
           })
         })  
     }
+
+const removeItem = (id) => {
+ setProducts (products.filter(p=> p.id!==id));
+}
+const checkout = async ()=>{
+  const response = await fetch ('http://192.168.0.200:3000/products')
+  {
+method:"POST",
+ headers:{
+'Content-Type':'application/json'
+ },
+if(response.status ===200 ){
+ alert( 'WIN' );
+ 
     
      return (<Fragment>
         <h3 className=''>Shopping Cart</h3>
-        <p>3 items in your cart</p>
+        <p>4 items in your cart</p>
         <table className='table'>
         <thead>
             <tr>
@@ -41,12 +55,19 @@ import CartItem from './CartItem';
             </tr>
             </thead>
             <tbody>
-             {products.map(product=> <CartItem onChangeCount={ChangeItemCount} key={product}></CartItem>)}
+             {products.map(product=> 
+             <CartItem 
+             onRemove ={removeItem}
+             onChangeCount={ChangeItemCount} 
+             key={product.id}
+             product ={product}/>
+             )}
             </tbody>
             <tfoot>
                 <tr>
                     <td colSpan={6}>
-
+                    <h4 className=''>{products.reduce((acc,p)=> acc + p
+                    <button className='' onClick={checkout}>
                     </td>
                 </tr>
             </tfoot>
