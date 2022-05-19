d62a42434e79c58e8c8dde1466b5ea807dce36f7
snitolya <92542355+snitolya@users.noreply.github.com>
2022-05-19 21:10:01 +0300

    Update Main.js

--- Many pages and propType/Main.js
+++ Many pages and propType/Main.js
@@ -1,33 +1,30 @@
-import { Fragment } from "react";
 import { Link } from "react-router-dom";
-import React, { useState , useEffect} from 'react';
+import React, { useState , useEffect, Fragment } from 'react';
 
 
-function Main(props){
-    const [users, setUsers] = useState([]);
+function Main(){
+    const [posts, setPosts] = useState([]);
+
     useEffect ( () => {
         (async function fetchData(){
             const response = await fetch (`http://192.168.0.200:3000/posts`)
             const data = await response.json();
-            setUsers(data);
+            setPosts(data);
         })();
-    },[props.id])
+    },[])
 
  
-
   return(<Fragment>
       {posts.map(post=><article>
         <h3>{post.title}</h3>
-        <img src={post}></img>
-
+        <img src={post.poster} alt=''></img> 
+        <p>{post.intro}</p>
+        <Link to={`/posts/${post.id}`}>Posts</Link>
       </article>)}
-    <h1>Custom component</h1>
-    <nav>
-        <Link to="/">Main</Link>{" "}
-        <Link to="posts">Posts</Link>
-      </nav>
   </Fragment>)
 }
 
 
+export default Main ;
 Main.propType={}
+
